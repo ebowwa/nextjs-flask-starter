52b4ef672aaafe041ee2ebb13ad7ddc1b80f507a
ebowwa <81942069+ebowwa@users.noreply.github.com>
2024-01-21 01:13:07 +0000

    pray

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -1,3 +1,5 @@
+// Home.tsx
+
 import Image from 'next/image';
 import Header from './header';
 import Footer from './footer';
@@ -5,45 +7,21 @@ import FeatureLink from './featurelink';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center justify-between min-h-screen p-10 lg:p-24">
       <Header />
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        {/* ...other contents... */}
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
+      <section className="z-10 flex flex-col items-center w-full max-w-5xl space-y-8">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
           src="/concrete_guy.png"
           alt="meet concrete guy"
-          width={180}
-          height={37}
+          width={540}
+          height={96}
           priority
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <FeatureLink
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          title="Docs"
-          description="Find in-depth information about Next.js features and API."
-        />
-        <FeatureLink
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          title="Learn"
-          description="Learn about Next.js in an interactive course with quizzes!"
-        />
-        <FeatureLink
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          title="Templates"
-          description="Explore the Next.js 13 playground."
-        />
-        <FeatureLink
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          title="Deploy"
-          description="Instantly deploy your Next.js site to a shareable URL with Vercel."
-        />
-      </div>
+        <div className="grid w-full grid-cols-1 gap-6 mb-10 text-center lg:grid-cols-4 lg:text-left">
+          {/* FeatureLinks */}
+        </div>
+      </section>
       <Footer />
     </main>
   );
